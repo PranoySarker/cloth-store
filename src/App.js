4729bf0165ab5a84455e00d76017b5c6cf33b07f
PranoySarker <pranoysarker01@gmail.com>
2023-08-14 00:00:06 +0600

    implement outlet fom react-router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import Home from './components/routes/home/home.component';
-import { Route, Routes } from 'react-router-dom';
+import { Outlet, Route, Routes } from 'react-router-dom';
 
+const Navigation = () => {
+  return (
+    <>
+      <div>
+        <h1>THis is Navigation</h1>
+      </div>
+      <Outlet />
+    </>
+  );
+};
+
+const Shop = () => {
+  return (
+    <>
+      <h1>THis is Shop</h1>
+    </>
+  );
+};
 const App = () => {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Navigation />}>
+          <Route index element={<Home />} />
+          <Route path="shop" element={<Shop />} />
+        </Route>
       </Routes>
     </>
   );
