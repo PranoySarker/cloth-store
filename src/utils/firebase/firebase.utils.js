b4e9e71e988cdd920467670798a9386f8c792c82
PranoySarker <88031593+PranoySarker@users.noreply.github.com>
2023-09-02 15:19:05 +0600

    store user info in firestore

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,21 +1,27 @@
-import { initializeApp } from 'firebase/app';
+import { initializeApp } from "firebase/app";
 import {
   getAuth,
   signInWithRedirect,
   signInWithPopup,
   GoogleAuthProvider,
-} from 'firebase/auth';
+} from "firebase/auth";
 
-import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
+import {
+  getFirestore,
+  doc,
+  getDoc,
+  setDoc,
+  collectionGroup,
+} from "firebase/firestore";
 
 //  Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyBNhqaQPzdiinLNfM1VM624v54cp0UqlAo',
-  authDomain: 'cloth-store-b3216.firebaseapp.com',
-  projectId: 'cloth-store-b3216',
-  storageBucket: 'cloth-store-b3216.appspot.com',
-  messagingSenderId: '217134457255',
-  appId: '1:217134457255:web:aa0890ccaf15f361db1682',
+  apiKey: "AIzaSyBNhqaQPzdiinLNfM1VM624v54cp0UqlAo",
+  authDomain: "cloth-store-b3216.firebaseapp.com",
+  projectId: "cloth-store-b3216",
+  storageBucket: "cloth-store-b3216.appspot.com",
+  messagingSenderId: "217134457255",
+  appId: "1:217134457255:web:aa0890ccaf15f361db1682",
 };
 
 // Initialize Firebase
@@ -24,15 +30,40 @@ const firebaseApp = initializeApp(firebaseConfig);
 const provider = new GoogleAuthProvider();
 
 provider.setCustomParameters({
-  prompt: 'select_account',
+  prompt: "select_account",
 });
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
 
-// initiate firbase database
+// initiate firestore database
 export const db = getFirestore();
 
 export const createUserDocumentFromAuth = async (userAuth) => {
-  const userDocRef = doc(db, 'users');
+  const userDocRef = doc(db, "users", userAuth.uid);
+  // console.log(userDocRef);
+
+  const userSnapshot = await getDoc(userDocRef);
+  // console.log(userSnapshot);
+  // console.log(userSnapshot.exists());
+
+  // if does not exist
+  // create/set the data from user userauth in my collection
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (error) {
+      console.log("error creating the user", error.message);
+    }
+  }
+  // if user exist
+  // return userdocRef
+  return userDocRef;
 };
