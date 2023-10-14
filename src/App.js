e089eafcbed4e2befb90a3b5ce1851d4cd237282
PranoySarker <88031593+PranoySarker@users.noreply.github.com>
2023-10-14 17:36:48 +0600

    Implement checkout page with styles
    Here implemented the checkout page for all products quantity,number of item,delete item and also the styling of that page

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Route, Routes } from "react-router-dom";
 import Navigation from "./components/routes/navigation/navigation.component";
 import Authentication from "./components/routes/authentication/authentication.component";
 import Shop from "./components/routes/shop/shop.component";
-import CheckOut from "./components/checkout/checkout.component";
+import CheckOut from "./components/routes/checkout/checkout.component";
 
 const App = () => {
   return (
