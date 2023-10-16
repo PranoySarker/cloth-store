6f65ca96b6444e28838735b0e5bf470deee165b0
PranoySarker <88031593+PranoySarker@users.noreply.github.com>
2023-10-16 17:20:27 +0600

    add nested route on shop page
    added nested routes for all the different products but there happened clash on some styles.trying to fix it

--- src/components/routes/shop/shop.component.jsx
+++ src/components/routes/shop/shop.component.jsx
@@ -1,24 +1,15 @@
-import { useContext, Fragment } from "react";
-import { CategoriesContext } from "../../../contexts/categories.context";
-import ProductCard from "../../product-card/product-card.component";
+import { Route, Routes } from "react-router-dom";
+
 import "./shop.styles.scss";
+import CategoriesPreview from "../categories-preview/categories-preview.component";
+import Category from "../category/category.component";
 
 const Shop = () => {
-  const { categoriesMap } = useContext(CategoriesContext);
-
   return (
-    <Fragment>
-      {Object.keys(categoriesMap).map((title) => (
-        <Fragment key={title}>
-          <h1 className="title">{title}</h1>
-          <div className="products-container">
-            {categoriesMap[title].map((product) => (
-              <ProductCard key={product.id} product={product} />
-            ))}
-          </div>
-        </Fragment>
-      ))}
-    </Fragment>
+    <Routes>
+      <Route index element={<CategoriesPreview />} />
+      <Route path=":category" element={<Category />} />
+    </Routes>
   );
 };
 
