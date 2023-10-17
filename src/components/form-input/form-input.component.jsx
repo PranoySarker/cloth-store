1d15ac7cbbea0f8d172c5fc196385fb0f58e0899
PranoySarker <88031593+PranoySarker@users.noreply.github.com>
2023-10-17 17:27:23 +0600

    converted to styled components
    converted rest of the scss styles into styled compoents and do some changes to home component

--- src/components/form-input/form-input.component.jsx
+++ src/components/form-input/form-input.component.jsx
@@ -1,21 +1,16 @@
 import React from "react";
-import "./form-input.styles.scss";
+import { Group, FormInputLabel, Input } from "./form-input.styles";
 
 const FormInput = ({ label, ...otherProps }) => {
   return (
-    <div className="group">
-      <input className="form-input" {...otherProps} />
+    <Group>
+      <Input {...otherProps} />
       {label && (
-        <label
-          className={`${
-            otherProps.value.length > 0 ? `shrink` : ""
-          } form-input-label`}
-          htmlFor=""
-        >
+        <FormInputLabel shrink={otherProps.value.length}>
           {label}
-        </label>
+        </FormInputLabel>
       )}
-    </div>
+    </Group>
   );
 };
 
