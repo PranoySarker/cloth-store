8f75e24ce100655cb1d7fbe34111ef02bab0444e
PranoySarker <88031593+PranoySarker@users.noreply.github.com>
2023-10-18 17:48:09 +0600

    using useReducer in cartContext

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,4 +1,5 @@
 import { createContext, useState, useEffect, useReducer } from "react";
+import { createAction } from "../utils/reducer/reducer.utils";
 import {
   createUserDocumentFromAuth,
   onAuthStateChangedListener,
@@ -40,7 +41,7 @@ export const UserProvider = ({ children }) => {
   console.log(currentUser);
 
   const setCurrentUser = (user) => {
-    dispatch({ type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user });
+    dispatch(createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user));
   };
 
   const value = { currentUser, setCurrentUser };
